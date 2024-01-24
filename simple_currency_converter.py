4fb3d815111aa0c0b8cb579497602f057852f83d
aghee <agymunzala@gmail.com>
2024-01-24 20:53:29 +0300

    Added validation of user input,converter usable

--- simple_currency_converter.py
+++ simple_currency_converter.py
@@ -1,32 +1,45 @@
-import sys
 #1usd=160.77kes
 #Pending---Capture user input to cater for any currency amounts
 #try except for catching errors
-print('-------------------------Simple Currency Converter-------------------')
 #How to validate input from user???-should a negative number be converted?-edge cases??
 #user enters amount to convert, if Y/y print--HERE IS YOUR....then function executes
 #if N---enter a different amount?(y/n) if Y/y--Enter amount you wish to convert please
 #if N/n/any other input apart from Y/y--exit program
 #Useful resource:https://www.freecodecamp.org/news/python-exit-how-to-use-an-exit-function-in-python-to-stop-a-program/
-try:
-    k=float(input('Enter amount you wish to convert please:'))
-    print(f'Do you wish to convert{k} to KSH? Y OR N')
-    m=input('Y or N')
-    if m=='Y':
-        print('HERE IS YOUR CONVERTED CURRENCY.THANK YOU.')
-    else:
-        sys.exit()
-except:
-    print('Please check the values you have entered!!!')
+print('-------------------------Simple Currency Converter-------------------')
+
+while True:
+    try:
+        num=float(input('Enter amount to convert here: '))
+        if num<=0:
+            print('UNABLE TO CONVERT A ZERO OR NEGATIVE AMOUNT!!!ENTER AGAIN!!!')
+            continue
+        print('Amount to convert is: ',num)
+        
+    except ValueError:
+        print('Please enter a NUMERICAL value')
+        continue
+    num1=input(f'Do you wish to proceed with {num} entered?Y OR N :')
+    if num1=='y'or num1=='Y':
+        break
+        #print('Your amount is:',num)
+    elif num1 !='y' or num1!='Y':
+        p=input('Do you wish to enter a different amount? Y OR N :')
+        if p=='y' or p=='Y':
+            continue
+        else:
+            print('Exiting the currency calculator...GOODBYE!!!')
+            exit()
+
 def calc_converter(**curr):
     converted_curr=list()
 #iteration variables iterating over list of tuples of key-value pairs obtained from curr    
     for currency,rate in curr.items():
-        converted=k*rate
+        converted=num*rate
         converted_curr.append((currency,converted))
     #print(converted_curr)
     for currency,amount in converted_curr:
-        print(k,'from',currency,'to Ksh is:',round(amount,2))
+        print(num,'from',currency,'to Ksh is:',round(amount,2))
 curr={
     'USD':163.00,
     'UGX':0.043,
